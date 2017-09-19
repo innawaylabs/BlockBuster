0c1a9f2f90984dea72dc33dbca531d94a976833f
Ravi Kumar Mandala <ravikumar.mandala@gmail.com>
2017-09-18 18:19:18 -0700

    Actual initial commit

---
+++ BlockBuster/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  BlockBuster
+//
+//  Created by Ravi Mandala on 9/18/17.
+//  Copyright © 2017 Ravi Mandala. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
---
+++ BlockBuster/MoviesViewController.swift
@@ -0,0 +1,75 @@
+//
+//  MoviesViewController.swift
+//  BlockBuster
+//
+//  Created by Ravi Mandala on 9/18/17.
+//  Copyright © 2017 Ravi Mandala. All rights reserved.
+//
+
+import UIKit
+import AFNetworking
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var moviesTableView: UITableView!
+    
+    var movies: [NSDictionary]?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.moviesTableView.dataSource = self
+        self.moviesTableView.delegate = self
+        
+        let url = URL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")
+        let request = URLRequest(url: url!)
+        let session = URLSession(
+            configuration: URLSessionConfiguration.default,
+            delegate:nil,
+            delegateQueue:OperationQueue.main
+        )
+        
+        let task: URLSessionDataTask = session.dataTask(
+            with: request as URLRequest,
+            completionHandler: { (data, response, error) in
+                if let data = data {
+                    if let responseDictionary = try! JSONSerialization.jsonObject(
+                        with: data, options:[]) as? NSDictionary {
+                        self.movies = (responseDictionary["results"] as! [NSDictionary])
+                        self.moviesTableView.reloadData()
+                    }
+                }
+        });
+        task.resume()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if let movies = movies {
+            return movies.count
+        } else {
+            return 0
+        }
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        
+        let movie = movies![indexPath.row]
+        let title = movie["title"] as! String
+        let overview = movie["overview"] as! String
+        let posterPath = movie["poster_path"] as! String
+        let baseUrl = "https://image.tmdb.org/t/p/w500"
+        
+        let imageUrl = NSURL(string: baseUrl + posterPath)
+        
+        cell.titleLabel.text = title
+        cell.overviewLabel.text = overview
+        cell.posterImageView.setImageWith(imageUrl as! URL)
+        
+        return cell
+    }
+}
